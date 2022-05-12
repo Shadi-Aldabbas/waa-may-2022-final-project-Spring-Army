ae5b2acd633a5b5642dff03b162d329b7d489bae
shadidabbasoptimum <shadi.dabbas@optimumpartners.co>
2022-05-11 21:25:57 -0500

    modelmapper added

--- pmpBackend/src/main/java/com/project/pmp/service/impl/UserServiceImpl.java
+++ pmpBackend/src/main/java/com/project/pmp/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.project.pmp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
