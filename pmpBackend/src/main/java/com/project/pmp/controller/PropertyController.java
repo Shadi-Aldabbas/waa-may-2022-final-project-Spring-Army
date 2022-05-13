2153476da4b8c611722aea289f5577f6de054c43
shadidabbasoptimum <dabbasFirst@outlook.com>
2022-05-12 23:24:19 -0500

    github change name test

---
+++ pmpBackend/src/main/java/com/project/pmp/controller/PropertyController.java
@@ -0,0 +1,45 @@
+package com.project.pmp.controller;
+
+import com.project.pmp.dto.GenericResponse;
+import com.project.pmp.entity.Property;
+import com.project.pmp.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/properties")
+public class PropertyController {
+
+    @Autowired
+    private PropertyService propertyService;
+
+    @PostMapping
+    public GenericResponse save(@RequestBody Property p) {
+
+
+        var property = propertyService.save(p);
+
+        if(property.getId() > 0){
+            return  new GenericResponse("successfuly registered", 200, property);
+        }
+
+        return  new GenericResponse("property has failed to register", 500, null);
+    }
+    @DeleteMapping
+    public void deleteById(@RequestParam int p) {
+        propertyService.delete(p);
+    }
+    @GetMapping
+    public ResponseEntity<GenericResponse> getAll() {
+        var result = propertyService.getAll();
+
+        return ResponseEntity.ok(new GenericResponse(result.size()+" Property Found", 200,result));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<GenericResponse> getById(@PathVariable int id) {
+        GenericResponse result = new GenericResponse("success", 200, propertyService.getById(id));
+        return ResponseEntity.ok(result);
+    }
+
+}
