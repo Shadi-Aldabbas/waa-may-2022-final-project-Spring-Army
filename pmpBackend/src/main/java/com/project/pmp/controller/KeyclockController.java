47a4fc453803db3667d59f725cca88c41edaa39a
Hasim Yilmaz <hasim@hasimyilmaz.com>
2022-05-17 00:20:27 -0500

    top 10 Properties

--- pmpBackend/src/main/java/com/project/pmp/controller/KeyclockController.java
+++ pmpBackend/src/main/java/com/project/pmp/controller/KeyclockController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
--- pmpBackend/src/main/java/com/project/pmp/controller/PropertyController.java
+++ pmpBackend/src/main/java/com/project/pmp/controller/PropertyController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/properties")
+@CrossOrigin
 public class PropertyController {
 
     @Autowired
